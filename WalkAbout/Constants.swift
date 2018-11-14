e1dd110603cf579bb701f9d898191a14c7847ab3
Hannes Sverrisson <sverrisson@ruv.is>
2018-11-14 22:00:22 +0000

    Constants

---
+++ WalkAbout/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  WalkAbout
+//
+//  Created by Hannes Sverrisson on 14/11/2018.
+//  Copyright © 2018 Hannes Sverrisson. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let server = "https://ossur.com/"
+    static let path = "api/walkabout/"
+    static let accInterval = 100  // ms
+}
+
